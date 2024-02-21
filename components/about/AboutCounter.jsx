b70074ab5acbf5e05d923286c968df3364eb6548
Jainth-24 <jainth.a@increscotech.com>
2024-02-21 19:01:28 +0530

    fix: counter change

--- components/about/AboutCounter.jsx
+++ components/about/AboutCounter.jsx
@@ -5,7 +5,7 @@ function AboutCounter() {
 	useCountUp({ ref: 'experienceCounter', end: 1, duration: 2 });
 	useCountUp({ ref: 'githubStarsCounter', end: 20, duration: 2 });
 	useCountUp({ ref: 'feedbackCounter', end: 100, duration: 2 });
-	useCountUp({ ref: 'projectsCounter', end: 100, duration: 2 });
+	useCountUp({ ref: 'projectsCounter', end: 90, duration: 2 });
 
 	return (
 		<div className="mt-10 sm:mt-20 bg-primary-light dark:bg-ternary-dark shadow-sm">
