5d931b5c1aa90422b318c1b199a33ec420dc3002
Jainth-24 <jainth.a@increscotech.com>
2024-02-21 16:25:45 +0530

    first commit

---
+++ data/aboutMeData.js
@@ -0,0 +1,20 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const aboutMeData = [
+	{
+		id: uuidv4(),
+		bio: 'I am a passionate and skilled full stack developer with a strong foundation in frontend and backend technologies. My journey in the world of development began with a curiosity to understand how things work behind the scenes on the web. This curiosity has led me to explore and master a range of technologies, allowing me to create robust and user-friendly web applications.',
+	},
+	{
+		id: uuidv4(),
+		bio: 'I have a solid understanding of frontend technologies such as Next.js, React Native, and Tailwind CSS, which enable me to craft elegant and responsive user interfaces. On the backend, I am proficient in working with GraphQL, Node.js, and databases like MongoDB and PostgreSQL, ensuring seamless data management and application logic.',
+	},
+	{
+		id: uuidv4(),
+		bio: 'In addition to my technical skills, I have a keen eye for design and user experience, striving to create applications that not only function flawlessly but also delight users with their aesthetics and usability',
+	},
+	{
+		id: uuidv4(),
+		bio: 'I am excited about the endless possibilities that technology offers and am eager to contribute my skills and creativity to projects that make a meaningful impact. Whether its developing innovative solutions or collaborating with a team to bring ideas to life, I am always ready to take on new challenges and explore new horizons in the world of development.',
+	},
+];
---
+++ data/projectsData.js
@@ -0,0 +1,294 @@
+import { v4 as uuidv4 } from "uuid";
+import {
+  FiFacebook,
+  FiInstagram,
+  FiLinkedin,
+  FiTwitter,
+  FiYoutube,
+} from "react-icons/fi";
+
+export const projectsData = [
+  {
+    id: 1,
+    title: "SM Exports Ecommerce",
+    url: "https://smexports-eta.vercel.app/",
+    category: "Web Application",
+    img: "/images/SM-exports.png",
+    ProjectHeader: {
+      title: "SM Exports",
+      publishDate: "Jul 26, 2021",
+      tags: "UI / Frontend",
+    },
+    ProjectImages: [
+      {
+        id: uuidv4(),
+        title: "Google Health Platform",
+        img: "/images/SM-exports1.png",
+      },
+      {
+        id: uuidv4(),
+        title: "Google Health Platform",
+        img: "/images/SM-exports2.png",
+      },
+      {
+        id: uuidv4(),
+        title: "Google Health Platform",
+        img: "/images/SM-exports3.png",
+      },
+    ],
+    ProjectInfo: {
+      ClientHeading: "About Client",
+      CompanyInfo: [
+        {
+          id: uuidv4(),
+          title: "Name",
+          details: "SM Exports",
+        },
+        {
+          id: uuidv4(),
+          title: "Activity",
+          details: "Growing of crops; market gardening; horticulture",
+        },
+        {
+          id: uuidv4(),
+          title: "Website",
+          details: "https://smexports-eta.vercel.app/",
+        },
+      ],
+      ObjectivesHeading: "Objective",
+      ObjectivesDetails:
+        "To create an engaging and user-friendly e-commerce website using Storyblok and Next.js, allowing customers to easily browse and select products. The website should provide a seamless inquiry process via email, ensuring a smooth user experience. Additionally, the website should be mobile responsive to cater to a wide range of users.",
+      Technologies: [
+        {
+          title: "Tools & Technologies",
+          techs: ["Nextjs", "TailwindCSS", "JavaScript", "Storyblok"],
+        },
+      ],
+      ProjectDetailsHeading: "Product Details",
+      ProjectDetails: [
+        {
+          id: uuidv4(),
+          details:
+            "The objective of this project is to develop a fully functional e-commerce website using Storyblok and Next.js. The website will allow customers to browse a variety of products and easily select items for purchase. Additionally, customers will be able to inquire about products through an email inquiry feature, providing a seamless communication channel between customers and the business. The website will be designed to be mobile responsive, ensuring optimal user experience across different devices.",
+        },
+        {
+          id: uuidv4(),
+          details:
+            "Key Features:\n1. Product Catalog: Display a range of products with detailed descriptions and images.\n2. Product Selection: Allow customers to select products and add them to a cart for checkout.\n3. Email Inquiry: Provide a form for customers to submit inquiries about specific products via email.\n4. Mobile Responsiveness: Ensure the website is optimized for mobile devices to provide a consistent user experience.\n5. User Authentication: Implement user authentication to allow registered users to save their cart and track orders.\n6. Admin Dashboard: Include an admin dashboard to manage products, view inquiries, and track orders.\n7. Payment Gateway Integration: Integrate a payment gateway to facilitate secure online transactions.\n8. SEO Optimization: Optimize the website for search engines to improve visibility and attract more organic traffic.",
+        },
+        {
+          id: uuidv4(),
+          details:
+            "Technologies Used:\n- Storyblok: Content management system for managing website content.\n- Next.js: React framework for building the front-end of the website.\n- Node.js: Back-end server environment for handling server-side logic.\n- Express: Web application framework for Node.js, used for building the back-end API.\n- MongoDB: NoSQL database for storing product information, user data, and inquiries.\n- SMTP Server: To handle email inquiries and notifications.",
+        },
+        {
+          id: uuidv4(),
+          details:
+            "Outcome:\nThe project aims to deliver a robust and user-friendly e-commerce website that meets the needs of both customers and the business. The website will provide a seamless shopping experience, encourage customer engagement through inquiries, and ultimately drive sales for the business.\n\nTimeline:\nThe project is estimated to be completed in 2 weeks , including design, development, testing, and deployment phases.",
+        },
+      ],
+      //   SocialSharingHeading: "Share This",
+      // SocialSharing: [
+      // 	{
+      // 		id: uuidv4(),
+      // 		name: 'Twitter',
+      // 		icon: <FiTwitter />,
+      // 		url: 'https://twitter.com/realstoman',
+      // 	},
+      // 	{
+      // 		id: uuidv4(),
+      // 		name: 'Instagram',
+      // 		icon: <FiInstagram />,
+      // 		url: 'https://instagram.com/realstoman',
+      // 	},
+      // 	{
+      // 		id: uuidv4(),
+      // 		name: 'Facebook',
+      // 		icon: <FiFacebook />,
+      // 		url: 'https://facebook.com/',
+      // 	},
+      // 	{
+      // 		id: uuidv4(),
+      // 		name: 'LinkedIn',
+      // 		icon: <FiLinkedin />,
+      // 		url: 'https://linkedin.com/',
+      // 	},
+      // 	{
+      // 		id: uuidv4(),
+      // 		name: 'Youtube',
+      // 		icon: <FiYoutube />,
+      // 		url: 'https://www.youtube.com/c/StomanStudio',
+      // 	},
+      // ],
+    },
+  },
+  {
+    id: 2,
+    title: "Fluffy Town",
+    url: "fluffytown.co.in",
+    category: "Web Application",
+    img: "/images/fluffytown.png",
+    ProjectHeader: {
+      title: "Fluffy Town",
+      publishDate: "Feb 14, 2024",
+      tags: "Web Application",
+    },
+    ProjectImages: [
+      {
+        id: uuidv4(),
+        title: "Fluffy Town",
+        img: "/images/fluffytown1.png",
+      },
+      {
+        id: uuidv4(),
+        title: "Fluffy Town",
+        img: "/images/fluffytown2.png",
+      },
+      {
+        id: uuidv4(),
+        title: "Fluffy Town",
+        img: "/images/fluffytown3.png",
+      },
+    ],
+    ProjectInfo: {
+      ClientHeading: "About Client",
+      CompanyInfo: [
+        {
+          id: uuidv4(),
+          title: "Name",
+          details: "Fluffy Town",
+        },
+        {
+          id: uuidv4(),
+          title: "Services",
+          details: "Import and Exports of Graments",
+        },
+        {
+          id: uuidv4(),
+          title: "Website",
+          details: "https://fluffytown.co.in",
+        },
+      ],
+      ObjectivesHeading: "Objective",
+      ObjectivesDetails:
+        "To develop a modern and efficient e-commerce website using Next.js and Tailwind CSS, integrated with Shopify for product management and Razorpay as a secure payment gateway. The website aims to provide a seamless shopping experience for customers, with a focus on user-friendly navigation, visually appealing design, and reliable payment processing. The integration with Shopify ensures easy management of products and inventory, while Razorpay ensures secure and hassle-free transactions. Overall, the project aims to deliver a high-quality e-commerce platform that meets the needs of both customers and the business.",
+      Technologies: [
+        {
+          title: "Tools & Technologies",
+          techs: [
+            "NextJs",
+            "Shopify",
+            "Razorpay",
+            "Storefront",
+            "TailwindCSS",
+            "Mobile Responsive",
+          ],
+        },
+      ],
+      ProjectDetailsHeading: "Challenge",
+      ProjectDetails: [
+        {
+          id: uuidv4(),
+          details:
+            "The objective of this project is to develop a modern and efficient e-commerce website using Next.js and Tailwind CSS, integrated with Shopify for product management and Razorpay as a secure payment gateway. The website aims to provide a seamless shopping experience for customers, with a focus on user-friendly navigation, visually appealing design, and reliable payment processing. The integration with Shopify ensures easy management of products and inventory, while Razorpay ensures secure and hassle-free transactions. Overall, the project aims to deliver a high-quality e-commerce platform that meets the needs of both customers and the business.",
+        },
+        {
+          id: uuidv4(),
+          details:
+            "Key Features:\n1. Next.js and Tailwind CSS: Utilize these technologies for efficient and responsive front-end development.\n2. Shopify Integration: Integrate with Shopify to manage products, inventory, and orders.\n3. Razorpay Payment Gateway: Implement Razorpay for secure and reliable payment processing.\n4. User-Friendly Design: Focus on creating a visually appealing and easy-to-navigate website for a seamless shopping experience.\n5. Mobile Responsiveness: Ensure the website is optimized for mobile devices to reach a wider audience.\n6. Secure Transactions: Implement security measures to protect customer data and ensure secure transactions.",
+        },
+        {
+          id: uuidv4(),
+          details:
+            "Technologies Used:\n- Next.js: React framework for building the front-end.\n- Tailwind CSS: Utility-first CSS framework for styling.\n- Shopify: E-commerce platform for product management.\n- Razorpay: Payment gateway for secure transactions.\n- Node.js: Back-end server environment for handling server-side logic.\n- Express: Web application framework for Node.js, used for building the back-end API.",
+        },
+        {
+          id: uuidv4(),
+          details:
+            "Outcome:\nThe project aims to deliver a fully functional and user-friendly e-commerce website that provides a seamless shopping experience for customers. By integrating with Shopify and Razorpay, the website will offer efficient product management and secure payment processing. The website's design and functionality will be optimized for mobile devices, ensuring accessibility for a wide range of users. Overall, the project aims to meet the business goals of increasing sales and customer satisfaction.",
+        },
+      ],
+    },
+  },
+  {
+    id: 3,
+    title: "VVM Garments",
+    url: "https://vvmgarments.netlify.app/",
+    category: "Web Application",
+    img: "/images/vvmgarments.png",
+    ProjectHeader: {
+      title: "VVM Garments",
+      publishDate: "Jan 26, 2023",
+      tags: "UI / Frontend",
+    },
+    ProjectImages: [
+      {
+        id: uuidv4(),
+        title: "VVM Garments",
+        img: "/images/vvmgraments1.png",
+      },
+      {
+        id: uuidv4(),
+        title: "Kabul Project Management UI",
+        img: "/images/vvmgarments2.png",
+      },
+      {
+        id: uuidv4(),
+        title: "Kabul Project Management UI",
+        img: "/images/vvmgarments3.png",
+      },
+    ],
+    ProjectInfo: {
+      ClientHeading: "About Client",
+      CompanyInfo: [
+        {
+          id: uuidv4(),
+          title: "Name",
+          details: "VVM Garments",
+        },
+        {
+          id: uuidv4(),
+          title: "Services",
+          details: "Garments",
+        },
+        {
+          id: uuidv4(),
+          title: "Website",
+          details: "https://vvmgarments.netlify.app/",
+        },
+      ],
+      ObjectivesHeading: "Objective",
+      ObjectivesDetails:
+        "To develop a comprehensive e-commerce website using PHP, allowing clients to effectively sell their products online. The website will feature an intuitive user interface for clients to manage their products, track sales, and view analytics. The admin dashboard will provide easy product management capabilities, including adding new products, updating inventory, and monitoring sales performance. The primary goal of the project is to create a seamless platform for clients to showcase and sell their products, enhancing their online presence and boosting sales..",
+      Technologies: [
+        {
+          title: "Tools & Technologies",
+          techs: ["HTML", "CSS", "PHP"],
+        },
+      ],
+      ProjectDetailsHeading: "Challenge",
+      ProjectDetails: [
+        {
+          id: uuidv4(),
+          details:
+            "The objective of this project is to develop a comprehensive e-commerce website using PHP, allowing clients to effectively sell their products online. The website will feature an intuitive user interface for clients to manage their products, track sales, and view analytics. The admin dashboard will provide easy product management capabilities, including adding new products, updating inventory, and monitoring sales performance. The primary goal of the project is to create a seamless platform for clients to showcase and sell their products, enhancing their online presence and boosting sales.",
+        },
+        {
+          id: uuidv4(),
+          details:
+            "Key Features:\n1. Client Product Management: Provide clients with tools to add, update, and manage their products.\n2. Sales Tracking: Implement features to track sales, view order history, and generate reports.\n3. Admin Dashboard: Develop an admin dashboard for easy product management, inventory tracking, and sales analytics.\n4. User-friendly Interface: Create an intuitive and responsive user interface for clients and admin users.\n5. Security Measures: Implement security features to protect client and user data, and ensure secure transactions.\n6. Scalability: Design the website to be scalable, allowing for future growth and expansion.",
+        },
+        {
+          id: uuidv4(),
+          details:
+            "Technologies Used:\n- PHP: Server-side scripting language for web development.\n- MySQL: Relational database management system for data storage.\n- HTML/CSS: Front-end technologies for designing the user interface.\n- JavaScript: Programming language for client-side interactions and dynamic content.\n- Bootstrap: Front-end framework for responsive design.\n- Apache: Web server software for hosting the website.\n- AdminLTE: Admin dashboard template for designing the admin interface.",
+        },
+        {
+          id: uuidv4(),
+          details:
+            "Outcome:\nThe project aims to deliver a robust e-commerce website that meets the needs of clients looking to sell their products online. The website will provide clients with an easy-to-use platform to showcase their products, manage inventory, and track sales. The admin dashboard will offer comprehensive tools for managing products, tracking sales performance, and generating reports. Overall, the project aims to enhance clients' online presence and boost their sales through an efficient and user-friendly e-commerce platform.",
+        },
+      ],
+    },
+  },
+];
