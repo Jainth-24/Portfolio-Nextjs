68e9c309071d4a8f8ba222ae7ceaad802c23dfc8
Jainth-24 <jainth.a@increscotech.com>
2024-03-01 13:38:50 +0530

    fix: experience

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,38 @@
-import { neutral } from "tailwindcss/colors";
+const colors = require('tailwindcss/colors');
 
-export const content = [
-	"./pages/**/*.{js,ts,jsx,tsx}",
-	"./components/**/*.{js,ts,jsx,tsx}",
-	"node_modules/flowbite-react/lib/esm/**/*.js",
-];
-export const darkMode = "class";
-export const theme = {
-	extend: {
-		colors: {
-			// Light colors
-			"primary-light": "#F7F8FC",
-			"secondary-light": "#FFFFFF",
-			"ternary-light": "#f6f7f8",
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'node_modules/flowbite-react/lib/esm/**/*.js',
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				// Light colors
+				'primary-light': '#F7F8FC',
+				'secondary-light': '#FFFFFF',
+				'ternary-light': '#f6f7f8',
 
-			// Dark colors
-			"primary-dark": "#0D2438",
-			"secondary-dark": "#102D44",
-			"ternary-dark": "#1E3851",
+				// Dark colors
+				'primary-dark': '#0D2438',
+				'secondary-dark': '#102D44',
+				'ternary-dark': '#1E3851',
 
-			// Extended v3 color
-			gray: neutral,
-		},
-		container: {
-			padding: {
-				DEFAULT: "1rem",
-				sm: "2rem",
-				lg: "5rem",
-				xl: "6rem",
-				"2xl": "8rem",
+				// Extended v3 color
+				gray: colors.neutral,
+			},
+			container: {
+				padding: {
+					DEFAULT: '1rem',
+					sm: '2rem',
+					lg: '5rem',
+					xl: '6rem',
+					'2xl': '8rem',
+				},
 			},
 		},
 	},
+	plugins: [require('@tailwindcss/forms')],
 };
-export const plugins = [require("flowbite/plugin"), require("@tailwindcss/forms")];
