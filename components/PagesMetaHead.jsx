66de03614cedb6f931a9062a100dca4e61e8f4d0
Jainth-24 <jainth.a@increscotech.com>
2024-07-26 10:32:13 +0530

    feat: added clarity

--- components/PagesMetaHead.jsx
+++ components/PagesMetaHead.jsx
@@ -3,23 +3,29 @@ import Head from 'next/head';
 function PagesMetaHead({ title, keywords, description }) {
 	return (
 		<Head>
-			<meta
-				name="viewport"
-				content="width=device-width, initial-scale=1"
-			/>
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<meta name="keywords" content={keywords} />
 			<meta name="description" content={description} />
 			<meta charSet="utf-8" />
 			<link rel="icon" href="/favicon.ico" />
 			<title>{title}</title>
+			<script type="text/javascript">
+				{`
+					(function(c,l,a,r,i,t,y){
+						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+					})(window, document, "clarity", "script", "nd8ensjtll");
+				`}
+			</script>
 		</Head>
 	);
 }
 
 PagesMetaHead.defaultProps = {
 	title: 'Jainth',
-	keywords: 'next.js developer, react developer, web, ui',
-	keywords: 'Full stack developer',
+	keywords: 'next.js developer, react developer, web, ui, Full stack developer',
+	description: 'Full stack developer'
 };
 
 export default PagesMetaHead;
